2d2abe67bb6054a576640dbbf3106e99ad0926c9
David Busch <busch@users.noreply.github.com>
2019-08-22 12:30:14 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,18 @@
+/*
+	David Busch
+	C++ Fall 2019
+	Due: August 29, 2019
+	Lab 1: Introduction to C++ Programming
+	This lab is a basic introduction to compiling a running a program in C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world! Blue";
+
+	return 0;
+}
